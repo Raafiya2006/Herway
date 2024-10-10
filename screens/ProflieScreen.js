ec6805965ab825fc2db514b2c51fa0a8f06b310b
Raafiya2006 <raafiyataskeen2006@gmail.com>
2024-10-10 16:34:08 +0530

    first commit

---
+++ screens/ProflieScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProflieScreen = () => {
+  return (
+    <View>
+      <Text>ProflieScreen</Text>
+    </View>
+  )
+}
+
+export default ProflieScreen
